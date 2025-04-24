58073de53fc22baec82ae19a3ed5b0a78394e62c
shahilkhan001 <mdshahilkhan59@gmail.com>
2025-04-24 10:39:43 +0530

    Updated my portfolio content

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Certifications from "./components/Certifications";
-import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 import "./index.css";
 
@@ -19,9 +18,7 @@ function App() {
       <Projects />
       <Certifications />
       <Contact/>
-      <FaGithub />
-      <FaLinkedin />
-
+      
     </div>
   );
 }
